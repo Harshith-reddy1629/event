6c802c70797fa365230297ac22d79609d18e2a78
Harshith-reddy1629 <reddybharshith3@gmail.com>
2024-06-04 09:25:56 +0530

    @0.0.6.1

--- src/context/apiInstance.js
+++ src/context/apiInstance.js
@@ -6,9 +6,25 @@ const token = Cookies.get("token");
 
 const api = axios.create({
   baseURL: "https://event-be.vercel.app",
+
   headers: {
     authorization: `Bearer ${token}`,
   },
 });
+
+api.interceptors.response.use(
+  function (response) {
+    console.log("Request Successful");
+
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      Cookies.remove("token");
+      console.log("invalid user");
+      window.location.replace("/login");
+    }
+  }
+);
 
 export default api;
